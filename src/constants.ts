0823685ce3180b7685fb69c47de2b3a07465bbfd
srbaron <stevenbaron.dev@gmail.com>
2025-04-26 15:07:28 -0700

    react-pixijs: adjust game size calcs

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,14 @@
-export const GAME_WIDTH = 896;
-export const GAME_HEIGHT = 736;
 export const TILE_SIZE = 32;
+export const TOTAL_ROWS = 23;
 export const TOTAL_COLS = 28;
+
+export const GAME_WIDTH = TILE_SIZE * TOTAL_COLS;
+export const GAME_HEIGHT = TILE_SIZE * TOTAL_ROWS;
 
 export const DEFAULT_X_POS = 250;
 export const DEFAULT_Y_POS = 450;
 
-export const PLAYER_MOVE_SPEED = 0.2;
+export const PLAYER_MOVE_SPEED = 0.1;
 
 export const CAMERA_ZOOM = 2;
 
