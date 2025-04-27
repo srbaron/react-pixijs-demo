2da8dbdcd1334dcb321c21853b616ca6f2087ed4
srbaron <stevenbaron.dev@gmail.com>
2025-04-27 13:41:21 -0700

    react-pixijs: added scene navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Application, extend } from "@pixi/react";
-import { Container, Graphics, Sprite } from "pixi.js";
+import { Container, Graphics, Sprite, TextStyle, Text } from "pixi.js";
 import { GAME_HEIGHT, GAME_WIDTH, TILE_SIZE } from "./constants";
 import styled from "styled-components";
 import { PlayerCharacter } from "./components/PlayerCharacter";
@@ -13,6 +13,7 @@ extend({
   Container,
   Graphics,
   Sprite,
+  Text,
 });
 
 const Center = styled.div`
@@ -21,8 +22,25 @@ const Center = styled.div`
   justify-content: center;
 `;
 
+enum ScenesEnum {
+  MAIN_MENU = "MAIN_MENU",
+  GAME = "GAME",
+}
+
+const textStyle = new TextStyle({
+  fontFamily: "Arial",
+  fontSize: 36,
+  fontStyle: "italic",
+  fontWeight: "bold",
+  fill: "#FFFFFF",
+  stroke: { color: "#4a1850", width: 5, join: "round" },
+});
+
 const App = () => {
   const [playerPos, setPlayerPos] = useState<Position>({ x: 0, y: 0 });
+  const [currentScene, setCurrentScene] = useState<ScenesEnum>(
+    ScenesEnum.MAIN_MENU,
+  );
   const canvas: Canvas = { width: GAME_WIDTH, height: GAME_HEIGHT };
   const updatePlayerPos = useCallback((x: number, y: number) => {
     setPlayerPos({
@@ -32,15 +50,35 @@ const App = () => {
   }, []);
 
   return (
-    <Center>
-      <Application width={canvas.width} height={canvas.height}>
-        <Camera playerPos={playerPos} canvasSize={canvas}>
-          <Level1 />
-          <Chest />
-          <PlayerCharacter movePlayer={updatePlayerPos} />
-        </Camera>
-      </Application>
-    </Center>
+    <>
+      <div>
+        <button onClick={() => setCurrentScene(ScenesEnum.GAME)}>
+          Swap to Game
+        </button>
+        <button onClick={() => setCurrentScene(ScenesEnum.MAIN_MENU)}>
+          Swap to Menu
+        </button>
+      </div>
+      <Center>
+        <Application width={canvas.width} height={canvas.height}>
+          {currentScene === ScenesEnum.MAIN_MENU ? (
+            <pixiText
+              text="PLAY GAME"
+              x={GAME_WIDTH / 2}
+              y={GAME_HEIGHT / 2}
+              style={textStyle}
+            />
+          ) : null}
+          {currentScene === ScenesEnum.GAME ? (
+            <Camera playerPos={playerPos} canvasSize={canvas}>
+              <Level1 />
+              <Chest />
+              <PlayerCharacter movePlayer={updatePlayerPos} />
+            </Camera>
+          ) : null}
+        </Application>
+      </Center>
+    </>
   );
 };
 
